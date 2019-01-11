27e430256e9528b1d075119b1eb44d05049cd6c3
Thorsten Woywod <tw@web-computing.de>
2019-01-11 13:50:22 +0100

    export validationsInjectionNames

--- src/assistant-validations.ts
+++ src/assistant-validations.ts
@@ -2,3 +2,4 @@ export * from "./components/validations/public-interfaces";
 export { descriptor } from "./components/validations/descriptor";
 export { PromptStateMixin } from "./components/validations/prompt-state-mixin";
 export { needs } from "./components/validations/annotations";
+export { validationsInjectionNames } from "./components/validations/injection-names";
