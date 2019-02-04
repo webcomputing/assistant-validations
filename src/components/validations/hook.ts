da23871286220e3eed93b9e7d5716c93919088c1
Antonius Ostermann <ostermann.antonius@gmail.com>
2019-02-04 11:23:11 +0100

    restructures hook to be more extendable and to use validations initializer correctly

--- src/components/validations/hook.ts
+++ src/components/validations/hook.ts
@@ -4,7 +4,7 @@ import { inject, injectable } from "inversify";
 import { validationsInjectionNames } from "../../../src/components/validations/injection-names";
 import { decoratorSymbols } from "./decorators";
 import { COMPONENT_NAME } from "./private-interfaces";
-import { DecoratorContent } from "./public-interfaces";
+import { DecoratorContent, InitializerOptions } from "./public-interfaces";
 import { ValidationsInitializer } from "./validations-initializer";
 
 @injectable()
@@ -20,34 +20,67 @@ export class BeforeIntentHook {
   }
 
   public execute: Hooks.BeforeIntentHook = async (mode, state, stateName, intent, machine, ...args: any[]) => {
-    const neededParams = this.retrieveNeededParamsFromMetadata(state, intent);
+    // First of all, check if pormpt is needed.
+    const promptNeeded = await this.needsPrompt(state, intent, args);
+    if (promptNeeded !== false) {
+      // If so, start prompting using validations initializer...
+      this.logger.info(`Missing required entity ${promptNeeded.unknownEntity} in current entity store.`);
+      await this.validationsInitializer.initializePrompt(stateName, intent, promptNeeded.unknownEntity, promptNeeded.promptOptions);
 
-    if (neededParams && neededParams.entities.length > 0) {
-      const unknownParam = neededParams.entities.filter(p => !this.currentRequestHasParam(p))[0];
+      // ... and return false to interrupt state machine
+      return false;
+    }
 
-      if (typeof unknownParam !== "undefined") {
-        this.logger.info(`Missing required entity ${unknownParam} in current entity store.`);
-        await this.validationsInitializer.initializePrompt(stateName, intent, unknownParam, {
-          promptStateName: neededParams.promptStateName,
-          redirectArguments: args,
-        });
-        return false;
+    // Secondly, do the same for confirmation
+    const confirmationNeeded = await this.initializeConfirmationIfNeeded();
+    if (confirmationNeeded !== false) {
+      // ... and return false to interrupt state machine
+      return false;
+    }
+
+    // If no validation / transition is needed, tell state machine to continue
+    return true;
+  };
+
+  private async initializeConfirmationIfNeeded(): Promise<boolean> {
+    return false;
+  }
+
+  /** Checks if we need to prompt for entities, and if so, returns information about the prompting we need, ready to use for validationsInitializer. Returns false otherwise. */
+  private async needsPrompt(
+    state: State.Required,
+    intentMethod: string,
+    args: any[]
+  ): Promise<false | { unknownEntity: string; promptOptions: Partial<InitializerOptions.Prompt> }> {
+    const decoratorContent = this.retrieveNeededEntitiesFromMetadata(state, intentMethod);
+
+    if (decoratorContent && decoratorContent.entities.length > 0) {
+      const unknownEntity = decoratorContent.entities.filter(p => !this.currentRequestHasEntities(p))[0];
+
+      if (typeof unknownEntity !== "undefined") {
+        return {
+          unknownEntity,
+          promptOptions: {
+            promptStateName: decoratorContent.promptStateName,
+            redirectArguments: args,
+          },
+        };
       }
     }
 
-    return true;
-  };
+    return false;
+  }
 
   /**
    * Checks if current request already has the given parameter
    * @param parameter The parameter to check existance of
    */
-  private currentRequestHasParam(entities: string): boolean {
+  private currentRequestHasEntities(entities: string): boolean {
     return this.entities.contains(entities);
   }
 
-  /** Sets this.neededParams based on Reflect.getMetadata result = based from what is stored into @needsEntities(..) */
-  private retrieveNeededParamsFromMetadata(target: State.Required, method: string): DecoratorContent.NeedsEntity | undefined {
+  /** Returns needed entities based on Reflect.getMetadata result = based from what is stored into @needsEntities(..) */
+  private retrieveNeededEntitiesFromMetadata(target: State.Required, method: string): DecoratorContent.NeedsEntity | undefined {
     if (typeof target === "undefined" || typeof method === "undefined" || typeof target[method] === "undefined") return undefined;
     const neededParams: DecoratorContent.NeedsEntity = Reflect.getMetadata(decoratorSymbols.needsEntities, target[method]);
 
